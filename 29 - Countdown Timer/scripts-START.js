ca30af246cb7197d061a0126c991f6ae13cf79db
Lobst3rG3off <erip.pasei@gmail.com>
2020-01-02 14:38:08 -0500

    finished timer

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,14 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left'); 
 const endTime = document.querySelector('display__end-time');
+const buttons = document.querySelectorAll('data-time')
+
 
 function timer(seconds) {
+//clear any existing timers
+
+clearInterval(countdown);
+
  const now = Date.now();
  const then = now + seconds * 1000;
  displayTimeLeft(seconds); 
@@ -34,6 +40,21 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
     const end = new Date(timestamp);
     const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour
     const minutes = end.getMinutes();
-    endTime.textContent = `Be Back at ${hour}:${minutes}`
+    endTime.textContent = `Be Back at ${adjustedHour}:${minutes < 10 ? '0' : '' }${minutes}`
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click' , startTimer));
+document.customForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
